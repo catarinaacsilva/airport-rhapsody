b3c982081e10c25c3b343c751b332979fb5f7dcb
Catarina Silva <c.alexandracorreia@ua.pt>
2020-02-20 18:02:02 +0000

    Add project with threads

---
+++ airport/src/BusDriver.java
@@ -0,0 +1,7 @@
+public class BusDriver implements Runnable{
+
+    @Override
+    public void run(){
+
+    }
+}
---
+++ airport/src/Porter.java
@@ -0,0 +1,7 @@
+public class Porter implements Runnable{
+
+    @Override
+    public void run(){
+
+    }
+}
